dad9e65c946576d0bf95788cf7524111418395e9
Artur Minin <explosionggg@mail.ru>
2020-06-27 08:34:41 +0300

    add course's page

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,20 @@
 const { Router } = require('express')
-const Course = require('../models/course')
 const router = Router()
+
+const Course = require('../models/course')
 
 router.get('/', async (req, res) => {
   const courses = await Course.getAll()
   res.render('courses', { title: 'Courses', isOnCoursesPage: true, courses })
+})
+
+router.get('/:id', async (req, res) => {
+  const course = await Course.getById(req.params.id)
+  res.render('course', {
+    layout: 'empty',
+    title: course.title,
+    course
+  })
 })
 
 module.exports = router
